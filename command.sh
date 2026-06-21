b34da85f39d4e4d1cafdcbce522fdd2a821b5a0d
Suchith Prabhu <suchith720@gmail.com>
2026-06-21 03:52:12 -0700

    added more commands

--- command.sh
+++ command.sh
@@ -2,3 +2,6 @@
 python scripts/01_conflation.py --pred_file /data/datasets/msmarco/XC/entity-gpt_ngame_trn_X_Y.npz --lbl_file //data/datasets/msmarco/XC/raw_data/entity_gpt.raw.txt  --trn_file /data/datasets/msmarco/XC/entity_gpt_trn_X_Y.npz --tst_file /data/datasets/msmarco/XC/entity_gpt_tst_X_Y.npz --topk 3 --batch_size 1024 --min_thresh 2 --max_thresh 100 --freq_thresh 50 --score_thresh 25 --diff_thresh 0.1 --embed_file /data/outputs/mogicX/01-msmarco-gpt-entity-linker-001/predictions/label_repr_full.pth
 
 python scripts/45-beir_fact_prediction.py --datasets hotpotqa --model_name nomic-ai/nomic-embed-text-v1 --model_type nomic --batch_size 512 --use_data_parallel --metric_dir /data/suchith/outputs/benchmarks/02-nomic_embed_text_v1/
+
+python scripts/43-evaluate_beir.py --datasets all --model_name Qwen/Qwen3-Embedding-0.6B --model_type qwen --batch_size 512 --use_data_parallel --metric_dir /data/suchith/outputs/benchmarks/04-qwen_embedding_0.6B/
+
